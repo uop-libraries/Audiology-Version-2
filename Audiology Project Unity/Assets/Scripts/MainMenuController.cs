39ee5c8c9a4b8dc4d3a3dad0baf5d6ba93274c0d
VeasnaLing <v_ling@u.pacific.edu>
2023-02-16 14:48:56 -0800

    refactoring code
    -move counseling button functionality into MainMenuController

--- Audiology Project Unity/Assets/Scripts/MainMenuController.cs
+++ Audiology Project Unity/Assets/Scripts/MainMenuController.cs
@@ -1,11 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    private GameObject _case1CounselingObject;
+    private GameObject _case2CounselingObject;
+
+    private TextMeshProUGUI _case1CounselingText;
+    private TextMeshProUGUI _case2CounselingText;
+
+    private Animator _case1CounselingAnimator;
+    private Animator _case2CounselingAnimator;
+
+    private Button _case1CounselingButton;
+    private Button _case2CounselingButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _case1CounselingObject = GameObject.Find("Case_1_Counseling_Button");
+        _case2CounselingObject = GameObject.Find("Case_2_Counseling_Button");
+        
+        _case1CounselingText = _case1CounselingObject.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+        _case2CounselingText = _case1CounselingObject.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+
+        _case1CounselingAnimator = _case1CounselingObject.GetComponent<Animator>();
+        _case2CounselingAnimator = _case2CounselingObject.GetComponent<Animator>();
+
+        _case1CounselingButton = _case1CounselingObject.GetComponent<Button>();
+        _case2CounselingButton = _case2CounselingObject.GetComponent<Button>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        SetCounselingButton();
+    }
+    
+    private void SetCounselingButton()
+    {
+        if (!StateNameController.isCase1HistoryDone)
+        {
+            _case1CounselingAnimator.enabled = false;
+            _case1CounselingButton.interactable = false;
+            _case1CounselingText.text = "Case 1 Counseling (Lock)";
+        }
+        else
+        {
+            _case1CounselingAnimator.enabled = true;
+            _case1CounselingButton.interactable = true;
+            _case1CounselingText.text = "Case 1 Counseling";
+        }
+        
+        if (!StateNameController.isCase2HistoryDone)
+        {
+            _case2CounselingAnimator.enabled = false;
+            _case2CounselingButton.interactable = false;
+            _case2CounselingText.text = "Case 2 Counseling (Lock)";
+        }
+        else
+        {
+            _case2CounselingAnimator.enabled = true;
+            _case2CounselingButton.interactable = true;
+            _case2CounselingText.text = "Case 2 Counseling";
+        }
+    }
+    
     // Case 1 and 2 button used this function in OnClick() event
     public void LoadScene(int scene)
     {

f922c5bc40b89c271ae2e80e80f15d69e069440f
VeasnaLing <v_ling@u.pacific.edu>
2023-04-19 17:28:31 -0700

    Fixed issues number 8 from client feedback

--- Audiology Project Unity/Assets/Scripts/CaseTwoHistory.cs
+++ Audiology Project Unity/Assets/Scripts/CaseTwoHistory.cs
@@ -148,7 +148,7 @@ public class CaseTwoHistory : MonoBehaviour {
                 break;
             case 6:
                 _nextInstruction = c2HInstruction06;
-                GoToInstructionNumber(panelNumber);
+                GoToInstruction06();
                 break;
             case 7:
                 // _nextInstruction = c2HInstruction07;
@@ -259,6 +259,74 @@ public class CaseTwoHistory : MonoBehaviour {
         _counter = 0;
     }
     
+     // Instruction for Tinnitus option
+    private void GoToInstruction06() {
+        _counter = 0;
+        _nextInstruction = c2HInstruction06;
+        var child1 = _nextInstruction.transform.GetChild(1).gameObject;
+        var child2 = _nextInstruction.transform.GetChild(2).gameObject;
+
+        child2.SetActive(false);
+
+        if (_isFirstTime) {
+            foreach (Transform child in child1.transform) {
+                child.gameObject.SetActive(false);
+            }
+        }
+
+        _isFirstTime = false;
+
+        foreach (Transform child in child1.transform) {
+            if (child.gameObject.name is "Option_A" or "Option_B" or "Option_C") {
+                child.gameObject.SetActive(true);
+            }
+
+            if (child.gameObject.GetComponent<Image>().color == Color.grey) {
+                child.gameObject.SetActive(false);
+                if (child.gameObject.activeSelf == false) {
+                    _counter++;
+                }
+            }
+
+            switch (_counter) {
+                case 1: {
+                    if (child.gameObject.name == "Option_D") {
+                        child.gameObject.SetActive(true);
+                    }
+
+                    break;
+                }
+                case 2: {
+                    if (child.gameObject.name == "Option_E") {
+                        child.gameObject.SetActive(true);
+                    }
+
+                    break;
+                }
+                case 3: {
+                    if (child.gameObject.name == "Option_F") {
+                        child.gameObject.SetActive(true);
+                    }
+
+                    break;
+                }
+                case 4: {
+                    if (child.gameObject.name == "Option_G") {
+                        child.gameObject.SetActive(true);
+                    }
+
+                    break;
+                }
+                case 7: {
+                    child2.SetActive(true);
+                    _isFirstTime = true;
+                    break;
+                }
+            }
+        }
+        _counter = 0;
+    }
+    
     // Instruction for hearing abilities option
     private void GoToInstruction07() {
         _counter = 0;
@@ -331,10 +399,11 @@ public class CaseTwoHistory : MonoBehaviour {
 
                     break;
                 }
-                case 9:
+                case 9: {
                     child2.SetActive(true);
                     _isFirstTime = true;
                     break;
+                }
             }
         }
         _counter = 0;
